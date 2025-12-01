d2a2384f7d238557af3dda041bc184af0f2d43f0
Rakesh07778777 <bossrakesh43@gmail.com>
2025-12-01 23:46:48 +0530

    gsap-animations

--- portfolio-3/portfolio-3.js
+++ portfolio-3/portfolio-3.js
@@ -24,3 +24,71 @@
                 ease: 'power2.out'
             });
         });
+
+
+        // About section animations
+        gsap.from('.about-text', {
+            scrollTrigger: {
+                trigger: '.about-content',
+                start: 'top 75%'
+            },
+            opacity: 0,
+            x: -50,
+            duration: 0.8,
+            ease: 'power2.out'
+        });
+
+        gsap.from('.about-stats', {
+            scrollTrigger: {
+                trigger: '.about-content',
+                start: 'top 75%'
+            },
+            opacity: 0,
+            x: 50,
+            duration: 0.8,
+            ease: 'power2.out'
+        });
+
+        // Skills animation
+        gsap.utils.toArray('.skill-item').forEach((skill, index) => {
+            gsap.from(skill, {
+                scrollTrigger: {
+                    trigger: skill,
+                    start: 'top 85%'
+                },
+                opacity: 0,
+                y: 40,
+                duration: 0.7,
+                delay: index * 0.15,
+                ease: 'power2.out'
+            });
+        });
+
+        // Projects animation
+        gsap.utils.toArray('.project-item').forEach((project, index) => {
+            const image = project.querySelector('.project-image');
+            const info = project.querySelector('.project-info');
+
+            gsap.from(image, {
+                scrollTrigger: {
+                    trigger: project,
+                    start: 'top 80%'
+                },
+                opacity: 0,
+                scale: 0.95,
+                duration: 0.8,
+                ease: 'power2.out'
+            });
+
+            gsap.from(info, {
+                scrollTrigger: {
+                    trigger: project,
+                    start: 'top 80%'
+                },
+                opacity: 0,
+                y: 30,
+                duration: 0.8,
+                delay: 0.2,
+                ease: 'power2.out'
+            });
+        });
